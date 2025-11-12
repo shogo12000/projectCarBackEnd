e9b25e789cbd1a17c6a6818cfc39f823e546807b
sidney <shogo12000@hotmail.com>
2025-11-11 20:30:08 -0800

    fix cors

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 import express from "express";
 import cors from "cors";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+const allowedOrigins = [
+    "http://localhost:5174/",
+    "http://localhost:5174"
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        // Se não houver origin (ex: Postman) ou se estiver na lista de permitidos
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    }, credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Servidor rodando!");
+    res.status(200).json({ msg: "Servidor rodando!" });
 });
 
 const PORT = 3000;
