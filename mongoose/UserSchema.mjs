404c7a93ac5eff999c788aec358480391931d59f
sidney <shogo12000@hotmail.com>
2025-11-12 20:10:41 -0800

    feat: create UserSchema

---
+++ mongoose/UserSchema.mjs
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+    }
+})
